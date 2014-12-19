917237c2c035315d7449cec5fd066c8d887fc382
Dawson Botsford <DawsonBotsford@gmail.com>
2014-12-18 23:55:08 -0700

    Incorporate http get

--- spellingBee/main.py
+++ spellingBee/main.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
-from enchant.checker import SpellChecker
-checker = SpellChecker("en_US")
+import re
+import json 
+import enchant
+import urllib2
 
-checker.set_text("Tis is a rpository wher I purposly hav mispelled sevral importat wordss!")
+print "These are the contents in your README of interest:\n"
+print urllib2.urlopen("https://raw.githubusercontent.com/dawsonbotsford/mispell/master/README.md").read()
 
-for err in checker:
-  print err.word, " is not a word. How about ", checker.suggest(err.word)[0], "?"
+f = open('words.json', 'rw')
+print json.loads(f.read())[0]
 
+checker = enchant.Dict("en_US")
+myString = "Tis is a rpository wher I purposly hav mispelled sevral importat wordss!"
+print myString
+#myString = myString.replace(" rpository ", " repository ")
+splitUp = re.compile('\w+').findall(myString)
+
+for word in splitUp:
+  if (not checker.check(word)):
+    #print word, " is wrong. How about ", checker.suggest(word)[0], "?"
+    #if word.lower 
+    myString = re.sub(word, checker.suggest(word)[0], myString)
+ 
+print myString 
