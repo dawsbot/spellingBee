53622305b35d79c865f190d48dcda5f61086bd8a
dawsonbotsford <dawsonbotsford@gmail.com>
2014-12-23 01:07:02 -0500

    Training for new mappings and ignores implemented

--- spellingBee/main.py
+++ spellingBee/main.py
@@ -9,31 +9,42 @@
 
 import re
 import os
+import sys
 import subprocess
 import enchant
 import github3
 
 wordDict = {}
+ignoreDict = {}
 checker = enchant.Dict("en_US")
+usage = "USAGE: ./main.py [target_user] [target_repo]"
 
 #Create Github object
-fp = open("../keys.txt")
-USERNAME = fp.readline().rstrip() # put your github username here.
-PASSWORD = fp.readline().rstrip() # put your github password here.
+keys_file = open("../keys.txt")
+USERNAME = keys_file.readline().rstrip() # put your github username here.
+PASSWORD = keys_file.readline().rstrip() # put your github password here.
 g = github3.login(USERNAME, PASSWORD)
 
-repo_name = 'ChainLink'
+if (len(sys.argv) != 3):
+	print usage
+target_user = sys.argv[1]
+repo_name = sys.argv[2] 
 
 #Fork the repo of interest into github account
-target_repo = g.repository('alexwalling', repo_name)
+target_repo = g.repository(target_user, repo_name)
 target_fork = target_repo.create_fork()
 myString = target_fork.readme().decoded
 
 #Fill replacement dictionary from file
-with open("words.txt") as f:
+with open("../words/words.txt") as f:
   for line in f:
     (key, value) = line.split("->")
     wordDict[key] = value.rstrip()
+
+#Fill ignore dictionary from file
+with open("../words/ignore.txt") as f:
+  for line in f:
+    ignoreDict[line.rstrip()] = 1 #Provide dictionary value of 1 (using dictionary for quick hashing)
 
 print "These are the contents in your README of interest:\n"
 
@@ -42,12 +53,26 @@ splitUp = re.compile('\w+').findall(myString)
 
 for word in splitUp:
   if (not checker.check(word)):
-    #print word, " is wrong. How about ", checker.suggest(word)[0], "?"
-    #Only replace if the replacement is found within words.txt
+    #print "contents of ignoreDict: ", ignoreDict
+    if (word in ignoreDict): #Do not alert user if word is in ignore
+      print "word is in ignoredict. word is ", word
+      continue
+    #Only replace if the replacement is mapped within words.txt
     if (word in wordDict):
       myString = re.sub(word, wordDict[word], myString)
- 
-print myString 
+    else: #Allow user to input new word map
+      solution = raw_input("\n" + word + " unrecognized.\n1. i == add to ignore\n2. [word] == replacement for new mapping\n3. \'\' == none: ")
+      if (solution == '' or solution == ' '): #user does not want this mapped
+        continue
+      elif (solution == 'i'): #user wants to map new ignore
+        with open('../words/ignore.txt','a') as f: f.write(word + "\n")
+        ignoreDict[word] = 1 
+      else: #user wants to map new correction
+        with open('../words/words.txt','a') as f: f.write(word + "->" + solution + "\n")
+        wordDict[word] = solution
+        myString = re.sub(word, wordDict[word], myString)
+        
+print "Corrected version: \n", myString 
 
 #Clone the target repo locally
 clone_url = target_fork.clone_url
@@ -56,15 +81,20 @@ bashCommand = "git clone " + clone_url
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 process.wait()
 
-print bashCommand
-
 #Put new README text in place of old
 os.chdir(target_fork.name)
 f = open(target_fork.readme().name, 'w')
 f.write(myString)
 f.close()
 
-bashCommand = "git add -A && git status"
 process = subprocess.call("../spellingBee/spellingBee/gitItAll.sh", shell=True)
 
-target_repo.create_pull("Spelling Correction", "master", "dawsonbotsford:master", "Automated corrections from https://github.com/dawsonbotsford/spellingBee . If it's correct star the repo, if it's wrong, report an issue please!")
+target_repo.create_pull("Spelling Correction", "master", "dawsonbotsford:master", "Automated corrections from https://github.com/dawsonbotsford/spellingBee . If the correction is correct, star the repo, if it is wrong, report an issue!")
+
+os.chdir("..")
+bashCommand = "rm -rf " + repo_name 
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+process.wait()
+
+#close files
+keys_file.close()
